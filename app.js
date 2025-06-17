395ac83e3e5c63fc84c0928a442cc67997bcc11b
Yadubir <yadubirjha2022@vitbhopal.ac.in>
2025-06-17 18:18:45 +0530

    added endpoint

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://apiverse-2.onrender.com', credentials: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
